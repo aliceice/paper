0fec6e9e64ffa285fd00454906d9c7ced19f70f9
Eléna Ihde-Simon <elena.ihde-simon@posteo.de>
2017-03-24 19:00:14 +0100

    Initial commit

---
+++ src/main/java/de/aliceice/paper/Form.java
@@ -0,0 +1,10 @@
+package de.aliceice.paper;
+
+/**
+ * XXX
+ *
+ * @author Eléna Ihde-Simon (elena.ihde-simon@posteo.de)
+ * @version $Id$
+ */
+public final class Form {
+}
---
+++ src/test/java/de/aliceice/paper/FormTest.java
@@ -0,0 +1,13 @@
+package de.aliceice.paper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public final class FormTest {
+    
+    @Test
+    public void nothing() throws Exception {
+        assertTrue(true);
+    }
+}
