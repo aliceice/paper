def903d3d666474571ee37e3a75c67856ec8c5f2
Eléna Ihde-Simon <elena.ihde-simon@posteo.de>
2017-04-04 17:59:47 +0200

    Adjust LocalizedRule to use a given ResourceBundle
    Instead of directly getting the 'Rules' ResourceBundle the
    LocalizedRule now expects a ResourceBundle and provides
    a default constructor with the implicit 'Rules' ResourceBundle.
    
    Issue #8

--- src/main/java/de/aliceice/paper/LocalizedRule.java
+++ src/main/java/de/aliceice/paper/LocalizedRule.java
@@ -1,17 +1,27 @@
 package de.aliceice.paper;
 
 import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 
 public abstract class LocalizedRule implements Rule {
     
     @Override
     public String getDescription() {
-        return String.format(PropertyResourceBundle.getBundle("Rules")
-                                                   .getString(getClass().getSimpleName()),
+        return String.format(this.resourceBundle.getString(getClass().getSimpleName()),
                              getDescriptionArguments());
     }
     
     protected Object[] getDescriptionArguments() {
         return new Object[] {};
     }
+    
+    protected LocalizedRule() {
+        this(PropertyResourceBundle.getBundle("Rules"));
+    }
+    
+    protected LocalizedRule(ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+    }
+    
+    private final ResourceBundle resourceBundle;
 }
