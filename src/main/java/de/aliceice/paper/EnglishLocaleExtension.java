51309b257a5405b6084ba37f93d933edfd8c10be
Eléna Ihde-Simon <elena.ihde-simon@posteo.de>
2017-09-11 17:55:06 +0200

    Update jUnit to version 5.0.0
    This fixes #69

--- src/main/java/de/aliceice/paper/EnglishLocaleExtension.java
+++ src/main/java/de/aliceice/paper/EnglishLocaleExtension.java
@@ -3,20 +3,24 @@ package de.aliceice.paper;
 import java.util.Locale;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.ContainerExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
 
 public final class EnglishLocaleExtension implements BeforeAllCallback, AfterAllCallback {
     
     @Override
-    public void beforeAll(ContainerExtensionContext context) throws Exception {
-        this.beforeExecutionDefault = Locale.getDefault();
+    public void beforeAll(ExtensionContext context) throws Exception {
+        getStore(context).put("previousLocale", Locale.getDefault());
         Locale.setDefault(Locale.ENGLISH);
     }
     
     @Override
-    public void afterAll(ContainerExtensionContext context) throws Exception {
-        Locale.setDefault(this.beforeExecutionDefault);
+    public void afterAll(ExtensionContext context) throws Exception {
+        Locale.setDefault(getStore(context).get("previousLocale", Locale.class));
     }
     
-    private Locale beforeExecutionDefault;
+    private ExtensionContext.Store getStore(ExtensionContext context) {
+        return context.getStore(Namespace.create("de.aliceice.paper", getClass().getName()));
+    }
+    
 }
